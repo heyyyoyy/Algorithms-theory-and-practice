d4b88ceb76939e515a68d790a75f5915324c7267
heyyyoyy <heyyyoyy@gmail.com>
2018-12-24 14:39:49 +0300

    small fix

--- Introduction: theory and tasks/last_digit_fib.rs
+++ Introduction: theory and tasks/last_digit_fib.rs
@@ -3,7 +3,7 @@ fn fib_digit(n: u32) -> u8 {
     assert!(n <= 10_u32.pow(7));
     let mut fib: (u8, u8) = (0, 1);
     for _ in 1..n {
-        fib = (fib.1 % 10, (fib.0 + fib.1) % 10);
+        fib = (fib.1, (fib.0 + fib.1) % 10);
     }
     fib.1
 }
